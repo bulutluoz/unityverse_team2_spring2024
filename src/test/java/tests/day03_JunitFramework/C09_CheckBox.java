f000758be07620bcb534a257208da2e5e19ad382
bulutluoz <abbulutluoz@gmail.com>
2024-03-24 15:50:31 +0100

    day03 sonu

--- src/test/java/tests/day03_JunitFramework/C09_CheckBox.java
+++ src/test/java/tests/day03_JunitFramework/C09_CheckBox.java
@@ -1,12 +1,57 @@
 package tests.day03_JunitFramework;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C09_CheckBox {
 
-    //Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın
-    //	a. Verilen web sayfasına gidin.
-    //	     https://testotomasyonu.com/form
-    //	b. Sirt Agrisi ve Carpinti checkbox’larini secin
-    //	c. Sirt Agrisi ve Carpinti checkbox’larininin seçili olduğunu test edin
-    //	d. Seker ve Epilepsi checkbox’larininin seçili
-    //     olmadigini test edin
+
+
+    @Test
+    public void checkboxTest() throws InterruptedException {
+        //Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //	a. Verilen web sayfasına gidin.
+        //	     https://testotomasyonu.com/form
+        driver.get("https://testotomasyonu.com/form");
+
+        //	b. Sirt Agrisi ve Carpinti checkbox’larini secin
+        //     secim hem yazidan hem de kutucuktan yapilabiliyor
+        //     ikisini de test edebilmek icin
+        //     sirt agrisini kutucuktan, carpintiyi yazidan secelim
+
+        WebElement sirtAgriKutusu = driver.findElement(By.id("gridCheck5"));
+        WebElement sirtAgriYaziElementi = driver.findElement(By.xpath("//*[@for='gridCheck5']"));
+
+        WebElement carpintiKutusu = driver.findElement(By.id("gridCheck4"));
+        WebElement carpintiYaziElementi = driver.findElement(By.xpath("//*[@for='gridCheck4']"));
+
+        sirtAgriKutusu.click();
+        carpintiYaziElementi.click();
+
+        //	c. Sirt Agrisi ve Carpinti checkbox’larininin seçili olduğunu test edin
+
+        Assert.assertTrue(sirtAgriKutusu.isSelected());
+        Assert.assertTrue(carpintiKutusu.isSelected());
+
+        //	d. Seker ve Epilepsi checkbox’larininin seçili olmadigini test edin
+
+        WebElement sekerKutusu = driver.findElement(By.id("hastalikCheck2"));
+        WebElement epilepsiKutusu = driver.findElement(By.id("hastalikCheck7"));
+
+        Assert.assertFalse(sekerKutusu.isSelected());
+        Assert.assertFalse(epilepsiKutusu.isSelected());
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
 }
